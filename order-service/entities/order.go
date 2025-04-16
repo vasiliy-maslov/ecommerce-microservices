9682fe6210b1036e0115419ee2b8f7fd31a6505d
Vasilii Maslov <gromovoi777@gmail.com>
2025-04-16 18:01:24 +1000

    add Order entity in entities package

---
+++ order-service/entities/order.go
@@ -0,0 +1,12 @@
+package entities
+
+import "time"
+
+type Order struct {
+	ID        string    // json:"id" db:"id"
+	UserID    string    // json:"user_id" db:"user_id"
+	Total     float64   // json:"total" db:"total"
+	Status    string    // json:"status" db:"status"
+	CreatedAt time.Time // json:"created_at" db:"created_at"
+	UpdatedAt time.Time // json:"updated_at" db:"updated_at"
+}
